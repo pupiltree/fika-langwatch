9759e96dca83368f55765da3273ff606d25d8ae9
Rahul Kumar <rahul20110@iiitd.ac.in>
2026-01-16 16:54:14 +0530

    v0.2.9: Update README - Manual Models as Option 1 (Recommended)

--- src/langwatch/__init__.py
+++ src/langwatch/__init__.py
@@ -27,7 +27,7 @@ from .key_manager import KeyManager, APIKey
 from .rate_limiter import InMemoryRateLimiter
 from .providers import ProviderFactory
 
-__version__ = "0.2.8"
+__version__ = "0.2.9"
 __author__ = "FIKA Private Limited"
 
 __all__ = [
